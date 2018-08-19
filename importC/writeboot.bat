3682333b7a5d89ac371ea11d69971ba7647bf7bf
PofeiShit <19143154@qq.com>
2018-08-19 18:23:36 +0800

    importC

---
+++ importC/writeboot.bat
@@ -0,0 +1,1 @@
+dd if=ipl10.bin of=\\.\Volume{9ea4600e-9d33-11e8-b017-206a8a5fed90} bs=512 count=1
---
+++ importC/writeharibote.bat
@@ -0,0 +1,1 @@
+dd if=haribote.sys of=\\.\Volume{9ea4600e-9d33-11e8-b017-206a8a5fed90} seek=59649 count=2880
