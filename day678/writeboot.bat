7a379be4615001bafb8d1d3b31b740a8c4f9b4da
PofeiShit <19143154@qq.com>
2018-09-02 16:08:51 +0800

    fisrt commit

---
+++ day678/writeboot.bat
@@ -0,0 +1,1 @@
+dd if=ipl10.bin of=\\.\Volume{19572513-924e-11e8-afeb-806e6f6e6963} bs=512 count=1
---
+++ day678/writeharibote.bat
@@ -0,0 +1,1 @@
+dd if=haribote.sys of=\\.\Volume{19572513-924e-11e8-afeb-806e6f6e6963} seek=32768 count=2880
